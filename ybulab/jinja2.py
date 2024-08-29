e6c7041af4727be6fa4cd82651653e76224425d5
Lianming Du <adullb@qq.com>
2024-08-29 20:24:36 +0800

    update

--- ybulab/jinja2.py
+++ ybulab/jinja2.py
@@ -1,10 +1,11 @@
+import os
 import urllib
 import hashlib
 
 from django.templatetags.static import static
 from django.urls import reverse
 from django.contrib.humanize.templatetags.humanize import apnumber, intcomma, intword, naturalday, naturaltime, ordinal
-
+from django.template.defaultfilters import time, date
 
 from jinja2 import Environment
 
@@ -13,6 +14,9 @@ def cravatar(email, size=40):
         hashlib.md5(email.lower().encode('utf-8')).hexdigest(),
         urllib.parse.urlencode({'s': str(size)})
     )
+
+def basename(path):
+    return os.path.basename(path)
 
 def environment(**options):
     env = Environment(**options)
@@ -30,5 +34,8 @@ def environment(**options):
         'naturalday': naturalday,
         'naturaltime': naturaltime,
         'ordinal': ordinal,
+        'basename': basename,
+        'time': time,
+        'date': date,
     })
     return env
